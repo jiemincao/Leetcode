84a5a4103945f2270723364dcb4abaaba6dbd8c8
Jiemin Git Lab <54890076+jiemincao@users.noreply.github.com>
2020-04-16 13:30:07 +0800

    Create 122_best_time_to_buy_and_sell_stock_II.c

---
+++ 122_best_time_to_buy_and_sell_stock_II.c
@@ -0,0 +1,12 @@
+int maxProfit(int* prices, int pricesSize)
+{
+    int sum = 0;
+    for(int i = 0; i < pricesSize-1; i++)
+    {
+        if(prices[i] < prices[i+1])
+        {
+            sum += prices[i+1] - prices[i];
+        }
+    }
+    return sum;
+}
